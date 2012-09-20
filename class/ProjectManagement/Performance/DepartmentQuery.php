4a1f0d02dd147e487d93a8c40a292c0515334d71
qusong <qusongdlzs@163.com>
2012-09-20 16:17:35 +0800

    am

--- class/ProjectManagement/Performance/DepartmentQuery.php
+++ class/ProjectManagement/Performance/DepartmentQuery.php
@@ -35,7 +35,6 @@ class DepartmentQuery extends OpenOaController{
 	}
 	
 	public function form(){
-		var_dump($_POST);exit;
 		$time = $this->params['select_time'];
 		$arrSelectTime = $this->getSelectTime($time);
 		
